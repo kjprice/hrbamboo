199909480abd87fd61859104372957a3c7c1f4f7
KJ Price <kjprice12@gmail.com>
2024-08-11 10:56:59 -0500

    Add test cases

--- product_match.py
+++ product_match.py
@@ -2,5 +2,29 @@
 
 from typing import List
 
+def _is_even(num: int) -> bool:
+    return num % 2 == 0
+
+def _are_all_even(nums: List[int]) -> bool:
+    return len(list(filter(_is_even, nums))) == len(nums)
+
+def _abs_diff(num1: int, num2: int) -> int:
+    return abs(num2 - num1)
+
 def match_product(product_supplies: List[int]) -> int:
+    # TODO: If two numbers are odd and not divisible by each other, return good
+    # TODO: If two numbers are odd and even, and not divisible by each other, return good
+    
+    # If any number is 1, impossible
+    if [num for num in product_supplies if num == 1]:
+        return -1
+    # If only one number exists
+    if len(product_supplies) == 1:
+        return -1
+    # If all numbers have a diff of 1, impossible
+    if len(product_supplies) == 2 and _abs_diff(*product_supplies) == 1:
+        return -1
+    # If all numbers are even, impossible
+    if _are_all_even(product_supplies):
+        return -1
     return sum(product_supplies) + 1
--- test/test_product_match.py
+++ test/test_product_match.py
@@ -3,5 +3,11 @@ from product_match import match_product
 from unittest import TestCase
 
 class TestProductMatch(TestCase):
-    def test_match_product(self):
-        self.assertEqual(match_product([1,2]), 4)
+    def test_diff_of_one(self):
+        self.assertEqual(match_product([3,4]), -1)
+    def test_all_even(self):
+        self.assertEqual(match_product([2, 4, 6, 8]), -1)
+    def test_support_includes_one_day(self):
+        self.assertEqual(match_product([1, 10]), -1)
+    def test_only_one_product(self):
+        self.assertEqual(match_product([3]), -1)
