37c78252224317aaabdd51b55ab5857d985cf0af
KJ Price <kjprice12@gmail.com>
2024-08-11 11:50:55 -0500

    Format and cleanup

--- product_match.py
+++ product_match.py
@@ -1,21 +1,17 @@
 #!/usr/bin/env python3
 
-from typing import Iterable, List
+from typing import List
 from helpers.validation import abs_diff, can_answer_be_found, prevalidation
 
 
-
 def match_product(product_supplies: List[int]) -> int:
-    # TODO: If two numbers are odd and not divisible by each other, return good
-    # TODO: If two numbers are odd and even, and not divisible by each other, return good
-
     prevalidation(product_supplies)
-    
+
     if not can_answer_be_found(product_supplies):
         return -1
     # If all numbers have a diff of 1, return (lowest value - 1)
     if len(product_supplies) == 2 and abs_diff(*product_supplies) == 1:
         return min(product_supplies) - 1
-    
+
     # TODO: Test [10, 5, 6]
     return sum(product_supplies) + 1
