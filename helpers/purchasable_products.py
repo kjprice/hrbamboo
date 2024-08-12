7b982951efdd7b5ec9199459a1ca047e1d40184c
KJ Price <kjprice12@gmail.com>
2024-08-11 19:34:13 -0500

    Fix error where some perfectly purchasable items were missing

--- helpers/purchasable_products.py
+++ helpers/purchasable_products.py
@@ -5,7 +5,7 @@ def _get_numbers_less_than_max(nums: List[int], max_number: int) -> List[int]:
     output = nums.copy()
     nums_added = set()
     i = 0
-    while output[i] <= max_number:
+    while i < max_number:
         num = output[i]
         for curr in nums:
             add = curr + num
@@ -14,7 +14,7 @@ def _get_numbers_less_than_max(nums: List[int], max_number: int) -> List[int]:
                 output.append(add)
         i += 1
     output.sort()
-    while output[-1] > max_number:
+    while output[-1] >= max_number:
         output.pop()
     return output
 
--- test/helpers/test_purchasable_products.py
+++ test/helpers/test_purchasable_products.py
@@ -5,7 +5,18 @@ from helpers.purchasable_products import _create_list_of_perfectly_purchasble_pr
 class TestPurchasableProducts(TestCase):
     def test_create_list_of_perfectly_purchasble_products(self):
         self.assertListEqual(
-            _create_list_of_perfectly_purchasble_products([2, 5]), [2, 4, 5, 6, 7, 8, 9, 10]
+            _create_list_of_perfectly_purchasble_products([2, 5]), [2, 4, 5, 6, 7, 8, 9]
         )
+        self.assertListEqual(
+            _create_list_of_perfectly_purchasble_products([3, 7]), [3, 6, 7, 9, 10, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+        )
+
     def test_find_last_missing_item(self):
         self.assertEqual(_find_last_missing_item([2, 4, 5, 6, 7, 8, 9, 10]), 3)
+
+    def test_find_largest_order_volume_not_perfectly_purchasable(self):
+        self.assertEqual(find_largest_order_volume_not_perfectly_purchasable([2, 5]), 3)
+        self.assertEqual(find_largest_order_volume_not_perfectly_purchasable([3, 5]), 7)
+        self.assertEqual(find_largest_order_volume_not_perfectly_purchasable([3, 7]), 11)
+        self.assertEqual(find_largest_order_volume_not_perfectly_purchasable([4, 7]), 17)
+        
