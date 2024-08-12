ff7bc1581ddb905a551a37b65a61348257bd1cae
KJ Price <kjprice12@gmail.com>
2024-08-12 09:38:52 -0500

    Allow for command line

--- product_match.py
+++ product_match.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
+import argparse
 from typing import List
+
 from helpers.validation import can_answer_be_found, prevalidation
 from helpers.purchasable_products import find_largest_order_volume_not_perfectly_purchasable
 
@@ -12,3 +14,24 @@ def match_product(product_supplies: List[int]) -> int:
         return -1
 
     return find_largest_order_volume_not_perfectly_purchasable(product_supplies)
+
+def _main():
+    def get_args():
+        parser = argparse.ArgumentParser(
+                        prog='./product_match.py',
+                        description='Largest Number Not Perfectly Purchasable'
+                        )
+        
+        parser.add_argument('supplies', metavar="N", type=int, nargs="+", help="supplies")
+        return parser.parse_args()
+    args = get_args()
+    print(f'Given the supplies: "{args.supplies}"')
+    output = match_product(args.supplies)
+    if output == -1:
+        print("We could not find the largest number not perfectly purchasable")
+    else:
+        print(f"We found the following largest number not perfectly purchasable: {output}")
+
+
+if __name__ == "__main__":
+    _main()
